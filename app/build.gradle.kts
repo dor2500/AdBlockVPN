0c2c8c5a2d0b2b9fdf0a9835d7ff8c01aa8c3e6f
dor2500 <50528935+dor2500@users.noreply.github.com>
2026-09-13 11:44:19 +0300

    Radar and Matrix Pulse animations (v7.5.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.adblocker.vpn.v2"
         minSdk = 26
         targetSdk = 33
-        versionCode = 748
-        versionName = "7.4.8"
+        versionCode = 75
+        versionName = "7.5.0"
         vectorDrawables.useSupportLibrary = true
         resourceConfigurations.addAll(listOf("en", "he", "iw"))
     }
